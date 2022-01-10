3be4f71bb565a4897ec0e3b41e2cb03384f30da6
Milica Djuric <milicadjuricrs@gmail.com>
2022-01-10 23:40:54 +0100

    Zadatak1

--- src/d10_01_2022/Zadatak1.java
+++ src/d10_01_2022/Zadatak1.java
@@ -3,8 +3,22 @@ package d10_01_2022;
 public class Zadatak1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Proizvod a= new Proizvod();
+		
+		a.productName="Shower gel";
+		a.productPrice=100;
+		a.productWeight=245;
+		
+		a.stampaj();
+		
+		Proizvod b= new Proizvod();
+		
+		b.productName="Shower gel";
+		b.productPrice=100;
+		b.productWeight=245;
+		
+		b.stampaj();
 	}
 
 }
