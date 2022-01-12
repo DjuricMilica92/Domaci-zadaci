d637b2ef062c31d98ace21a73a3fe507c3773522
Milica Djuric <milicadjuricrs@gmail.com>
2022-01-12 13:58:38 +0100

    Proizvod

--- src/d10_01_2022/Proizvod.java
+++ src/d10_01_2022/Proizvod.java
@@ -1,21 +1,10 @@
 package d10_01_2022;
 
-//Napisati klasu Proizvod koja ima atribute
-//naziv proizvoda (String)
-//cenu proizvoda (double)
-//težinu proizvoda u gramima. (double)
-//	i metode:
-//stampaj - stampa podatke proizvoda u formatu {{naziv}}, {{cena}}, {{tezina}}
-//
-//
-//U glavnom programu kreirati minimum 2 proizvoda i za svaki od njih pozvati metodu za stampu.
-
-
 public class Proizvod {
 
-	String productName;
-	double productPrice;
-	double productWeight;
+	public String productName;
+	public double productPrice;
+	public double productWeight;
 	
 	public void stampaj() {
 		System.out.println("{{" + this.productName+ "}} " + "{{"+ this.productPrice + " rsd"+"}} " +
