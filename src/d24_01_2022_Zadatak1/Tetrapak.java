ec4f9c5bd3231e929e4349657aa6b5e5de3ca7cc
Milica Djuric <milicadjuricrs@gmail.com>
2022-01-25 11:44:25 +0100

    Klasa Tetrapak

--- src/d24_01_2022_Zadatak1/Tetrapak.java
+++ src/d24_01_2022_Zadatak1/Tetrapak.java
@@ -2,7 +2,6 @@ package d24_01_2022_Zadatak1;
 
 public class Tetrapak extends Ambalaza {
 
-
 //	Kreirati klasu Tetrapak koja nasledjuje klasu Ambalaza koja ima:
 //		atribut koji kaze da li se moze reciklirati
 //		osnovna cena
@@ -15,11 +14,12 @@ public class Tetrapak extends Ambalaza {
 
 	private boolean reciklaza;
 	private double osnovnaCena;
-	
-	public Tetrapak(String barkod, String naziv, double netoTezina, double brutoTezina) {
+
+	public Tetrapak(String barkod, String naziv, double netoTezina, double brutoTezina, boolean reciklaza,
+			double osnovnaCena) {
 		super(barkod, naziv, netoTezina, brutoTezina);
-		this.reciklaza=reciklaza;
-		this.osnovnaCena=osnovnaCena;
+		this.reciklaza = reciklaza;
+		this.osnovnaCena = osnovnaCena;
 	}
 
 	public boolean isReciklaza() {
@@ -37,29 +37,29 @@ public class Tetrapak extends Ambalaza {
 	public void setOsnovnaCena(double osnovnaCena) {
 		this.osnovnaCena = osnovnaCena;
 	}
-	
-	
+
 //		ako je od recikliranog materijala, ( tezina pakovanja ) * 1.5 + (osnovna cena);
 //		ako nije u cenu ulazi samo osnovna cena
 //		metoda stampaj stampa sve podatke iz klase tetrapak.
-	
+
 	@Override
 	public double cenaArtikla() {
-		if(reciklaza) {
-			return tezinaPakovanja()*1.5 + osnovnaCena;
-		}else {
+		if (reciklaza) {
+			return tezinaPakovanja() * 1.5 + this.getOsnovnaCena();
+		} else {
 			return this.getOsnovnaCena();
 		}
 	}
-	
+
 	@Override
-	public void stampa () {
+	public void stampa() {
 		System.out.println("***Tetrapak***");
 		System.out.println("Bar kod: " + this.getBarkod());
 		System.out.println("Bruto: " + this.getBrutoTezina() + " gr.");
 		System.out.println("Neto: " + this.getNetoTezina() + " gr.");
 		System.out.println("Naziv: " + this.getNaziv());
-		System.out.println("Osnovna cena: " + this.getOsnovnaCena()+ " din.");
-		System.out.println("Cena artikla: " + cenaArtikla()+ " din.");
+		System.out.println("Osnovna cena: " + this.osnovnaCena + " din.");
+		System.out.println("Cena artikla: " + cenaArtikla() + " din.");
+		System.out.println();
 	}
 }
